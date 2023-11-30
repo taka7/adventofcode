5d2bf2b96cb6d3be5fce020f9a17ef8f143d10e3
Takayoshi Nobuoka <takayoshi.nobuoka@woven-planet.global>
2023-11-30 14:42:11 +0900

    feat: day1-1 calorie counting

--- 2022/day1/calorie-counting/src/main.rs
+++ 2022/day1/calorie-counting/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use itertools::Itertools;
+
+fn main() -> Result<(), std::io::Error> {
+    let lines = std::io::stdin().lines();
+
+    let m = lines
+        .into_iter()
+        .map(|v| v.unwrap())
+        .group_by(|elt| !elt.is_empty())
+        .into_iter()
+        .filter_map(|(k, v)| if k { Some(v) } else { None })
+        .map(|v| {
+            v.map(|elt| elt.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>()
+        })
+        .map(|v| v.iter().sum::<i32>())
+        .max();
+
+    println!("{}", m.unwrap());
+
+    Ok(())
 }
