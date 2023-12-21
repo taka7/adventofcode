2fbd3566b2047bab473271e70d0c621faf62e4f9
Takayoshi Nobuoka <takayoshi.nobuoka@woven-planet.global>
2023-12-21 17:01:22 +0900

    feat: day08 part2

--- 2023/day08-haunted-wasteland/src/main.rs
+++ 2023/day08-haunted-wasteland/src/main.rs
@@ -1,5 +1,8 @@
+use num::Zero;
+use std::cmp::Eq;
+use std::cmp::PartialOrd;
 use std::collections::HashMap;
-use std::hash::{Hash, Hasher};
+use std::ops::Rem;
 
 #[derive(Debug)]
 enum Direction {
@@ -13,31 +16,16 @@ struct Choice {
     right: String,
 }
 
-#[derive(Debug, Eq, PartialEq, Hash, Clone)]
+#[derive(Debug, Eq, PartialEq, Clone)]
 enum Marker {
     Start,
     End,
     No,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, PartialEq, Eq, Clone, Hash)]
 struct Key {
     id: String,
-    marker: Marker,
-}
-
-impl PartialEq for Key {
-    fn eq(&self, other: &Self) -> bool {
-        self.id.eq(&other.id)
-    }
-}
-
-impl Eq for Key {}
-
-impl Hash for Key {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.id.hash(state);
-    }
 }
 
 fn parse() -> (Vec<Direction>, HashMap<String, Choice>) {
@@ -101,74 +89,98 @@ fn part1(seq: &Vec<Direction>, maps: &HashMap<String, Choice>) -> usize {
     count + 1
 }
 
-fn part2(seq: &Vec<Direction>, maps_orig: &HashMap<String, Choice>) -> usize {
-    let maps: HashMap<Key, Choice> = HashMap::from_iter(maps_orig.iter().map(|(k, v)| {
-        let marker = match k.chars().nth(k.len() - 1).unwrap() {
-            'A' => Marker::Start,
-            'Z' => Marker::End,
-            _ => Marker::No,
-        };
-        (
-            Key {
-                id: k.clone(),
-                marker: marker,
-            },
-            v.clone(),
-        )
-    }));
+fn gcd<I>(a1: I, a2: I) -> I
+where
+    I: Rem<Output = I> + PartialOrd + Copy + Zero,
+{
+    let (mut a1, mut a2) = if a1 > a2 { (a1, a2) } else { (a2, a1) };
+    while !(a1 % a2).is_zero() {
+        a1 = a1 % a2;
+        (a1, a2) = if a1 > a2 { (a1, a2) } else { (a2, a1) }
+    }
+    a2
+}
 
-    let mut poses = vec![maps
-        .keys()
-        .filter_map(|k| {
-            if k.marker == Marker::Start {
-                Some(k.clone())
-            } else {
-                None
-            }
-        })
-        .collect::<Vec<Key>>()];
-    println!("poses: {:?}", poses);
-    while poses[poses.len() - 1]
-        .iter()
-        .any(|k| k.marker != Marker::End)
-    {
-        match seq.iter().try_fold(poses, |mut acc, dir| {
-            //            println!("Dir: {:?}", dir);
-            if acc[acc.len() - 1].iter().any(|k| k.marker != Marker::End) {
-                let iter = acc[acc.len() - 1].iter().map(|pos| {
-                    let choice = maps.get(pos).unwrap();
-                    let next = match dir {
-                        Direction::Left => &choice.left,
-                        Direction::Right => &choice.right,
-                    };
-                    //                    println!("pos: {:?} next:{:?}", pos, next);
-                    Key {
-                        id: next.clone(),
-                        marker: if next.chars().nth(next.len() - 1) == Some('Z') {
-                            Marker::End
-                        } else {
-                            Marker::No
-                        },
-                    }
-                });
-                acc.push(iter.collect::<Vec<Key>>());
-            }
-            Some(acc)
-        }) {
-            None => panic!("panic"),
-            Some(x) => poses = x,
-        }
-        //        println!("poses: {:?}, len:{}", poses, poses.len());
+fn gcd_from(seq: &Vec<i32>) -> i32 {
+    seq.iter().skip(1).fold(seq[0], |acc, s| gcd(acc, *s))
+}
+
+fn gcd_from_iter<'a, I, U>(mut seq: I) -> U
+where
+    I: Iterator<Item = U>,
+    U: Rem<Output = U> + PartialOrd + Copy + Zero,
+{
+    let first = seq.next().unwrap();
+    seq.fold(first, |acc, s| gcd(acc, s))
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_gcd() {
+        assert_eq!(1, gcd(2, 3));
+        assert_eq!(2, gcd(2, 8));
+        assert_eq!(5, gcd(105, 235));
+        assert_eq!(15, gcd(105, 225));
     }
 
-    poses.len() - 1
+    #[test]
+    fn test_gcd_from() {
+        assert_eq!(1, gcd_from(&vec![2, 3]));
+        assert_eq!(2, gcd_from(&vec![2, 8]));
+        assert_eq!(5, gcd_from(&vec![105, 235]));
+        assert_eq!(15, gcd_from(&vec![105, 225]));
+        assert_eq!(15, gcd_from(&vec![105, 225, 45]));
+        assert_eq!(1, gcd_from(&vec![105, 225, 44]));
+        assert_eq!(3, gcd_from(&vec![105, 225, 3]));
+    }
+}
+
+fn part2(seq: &Vec<Direction>, maps_orig: &HashMap<String, Choice>) -> usize {
+    let maps: HashMap<String, (Marker, Choice)> =
+        HashMap::from_iter(maps_orig.iter().map(|(k, v)| {
+            let marker = match k.chars().nth(k.len() - 1).unwrap() {
+                'A' => Marker::Start,
+                'Z' => Marker::End,
+                _ => Marker::No,
+            };
+            (k.clone(), (marker, v.clone()))
+        }));
+
+    let starts = maps
+        .iter()
+        .filter_map(|(k, v)| if v.0 == Marker::Start { Some(k) } else { None });
+
+    let counts = starts.map(|k| {
+        seq.iter()
+            .cycle()
+            .scan(k, |acc, s| {
+                let v = maps.get(*acc).unwrap();
+                let next = match s {
+                    Direction::Left => &v.1.left,
+                    Direction::Right => &v.1.right,
+                };
+                if v.0 == Marker::End {
+                    None
+                } else {
+                    *acc = next;
+                    Some(())
+                }
+            })
+            .count()
+    });
+
+    let gcd = gcd_from_iter(counts.clone());
+    counts.map(|v| v / gcd).product::<usize>() * gcd
 }
 
 fn main() -> Result<(), std::io::Error> {
     let (seq, maps) = parse();
 
-    //    println!("{:?}", part1(&seq, &maps));
-    println!("{:?}", part2(&seq, &maps));
+    println!("part1: {:?}", part1(&seq, &maps));
+    println!("part2: {:?}", part2(&seq, &maps));
 
     Ok(())
 }
